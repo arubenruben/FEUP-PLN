b4e6cca99b1b002e2fc0155614560b3341a95899
Rúben Almeida <almeidaruben35@gmail.com>
2022-04-02 11:08:26 +0100

    ssadda

--- src/main.py
+++ src/main.py
@@ -1,34 +1,20 @@
-from src.exploratory_analyses import class_distribution
+from src.exploratory_analyses import class_distribution, outlier_detection
 from src.models import baseline_2
 from src.other import load_dataset
 
 
 def main():
     df_adu, df_text = load_dataset()
-    baseline_2(df_adu)
+    # baseline_2(df_adu)
+
+    outlier_detection(df_adu)
+
     # class_distribution(df_adu)
     # print("Baseline")
     # test_different_features_sizes(df_adu.copy(), df_text.copy())
     # baseline(df_adu.copy(), df_text.copy())
     print("----------")
 
-
-"""
-    print("TF-IDF")
-    test_tf_idf(df_adu.copy(), df_text.copy())
-    print("----------")
-
-    print("One Hot Encoding")
-    test_1_hot_vector(df_adu.copy(), df_text.copy())
-    print("----------")
-    # for algorithm in algorithms:
-    #  print(algorithm)
-    # baseline(df_adu, df_text, algorithm=algorithm)
-    # print('---------------')
-
-# print(df_adu.head())
-# print(df_text.head())
-"""
 
 if __name__ == "__main__":
     main()
