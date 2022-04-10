24d7723262f5c38122813fa10c43e9978918e206
InesQuarteu <quarteui@gmail.com>
2022-04-10 16:23:44 +0100

    Back Translation

--- src/main.py
+++ src/main.py
@@ -3,16 +3,16 @@ from other import load_dataset
 
 
 def main():
-    # dev()
-    prod()
+    dev()
+    # prod()
 
 
 def dev():
     df_adu, df_text = load_dataset()
     # baseline_with_lexicons(df_adu)
     # baseline_with_pos(df_adu)
-    baseline_with_normalization(df_adu, df_text)
-    # baseline(df_adu, df_text, algorithm="decision_tree")
+    # baseline_with_normalization(df_adu, df_text)
+    baseline(df_adu, df_text)
     # baseline(df_adu, df_text)
 
     # test_1_hot_vector(df_adu, df_text)
